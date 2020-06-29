de7ccfe6a4c96ad6dff4126cfae89bc075d745bd
Htun Aung Kyaw <htunaungkyaw730@gmail.com>
2020-06-29 18:30:54 +0630

    new interactive files with req

--- index.js
+++ index.js
@@ -14,18 +14,27 @@
 
 
 //Create Server 
-// const http = require("http");
-// const path = require("path");
-// const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-// const server = http.createServer((req,res)=>{
-//     if(req.url==='/'){
-//         res.writeHead(200,{'Content-Type':'text/html'});
-//         res.write('<h1>Htun Aung</h1>');
-//         res.end();
-//     }
-// });
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type':'text/html'})
+            res.end(content);
+        })
+    }
+    if(req.url==='/about'){
+        fs.readFile(path.join(__dirname,'public','about.html'),(err,content)=>{
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type':'text/html'})
+            res.end(content);
+        })
+    }
+});
 
-// const PORT = process.env.PORT || 5005; 
+const PORT = process.env.PORT || 5005; 
 
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
