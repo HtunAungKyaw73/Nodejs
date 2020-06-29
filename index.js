22dff3b06b0a6efa5e620eff1d9817b7a169db9b
Htun Aung Kyaw <htunaungkyaw730@gmail.com>
2020-06-29 18:22:46 +0630

    server creation

--- index.js
+++ index.js
@@ -4,10 +4,28 @@
 
 // person.greeting();
 
-const Logger = require('./logger');
+// const Logger = require('./logger');
 
-const logger = new Logger();
+// const logger = new Logger();
 
-logger.on('message',(data)=>console.log('Called Listener: ', data));
+// logger.on('message',(data)=>console.log('Called Listener: ', data));
 
-logger.log('Hello');
+// logger.log('Hello');
+
+
+//Create Server 
+// const http = require("http");
+// const path = require("path");
+// const fs = require("fs");
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url==='/'){
+//         res.writeHead(200,{'Content-Type':'text/html'});
+//         res.write('<h1>Htun Aung</h1>');
+//         res.end();
+//     }
+// });
+
+// const PORT = process.env.PORT || 5005; 
+
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
