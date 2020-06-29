7108bb52edba81ad7a8ebe1d892cb2f87fd53119
Htun Aung Kyaw <htunaungkyaw730@gmail.com>
2020-06-29 17:11:43 +0630

    new

--- reference/event_demo.js
+++ reference/event_demo.js
@@ -4,9 +4,9 @@ class MyEmitter extends EventEmitter{}
 
 const myEmitter = new MyEmitter();
 
-myEmitter.on('event',()=> console.log('Event Fired'));
+myEmitter.on('event1',()=> console.log('Event Fired'));
 
-myEmitter.emit('event');
-myEmitter.emit('event');
-myEmitter.emit('event');
+myEmitter.emit('event1');
+myEmitter.emit('event1');
+myEmitter.emit('event1');
 myEmitter.emit('event');
