34a3fb4f8b08ffaa60aa27081dcc759a288e5687
Htun Aung Kyaw <htunaungkyaw730@gmail.com>
2020-06-29 17:37:00 +0630

    new files and test files

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const Person = require('./person');
+// const Person = require('./person');
 
-const person = new Person('Htun Aung Kyaw',21);
+// const person = new Person('Htun Aung Kyaw',21);
 
-person.greeting();
+// person.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message',(data)=>console.log('Called Listener: ', data));
+
+logger.log('Hello');
